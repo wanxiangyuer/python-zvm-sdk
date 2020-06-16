d9e8677a385d11d9b471071b2bf3070d8002855a
wgxoyun <wgxoyun@cn.ibm.com>
2020-06-16 14:21:57 +0800

    sles15 support in zcc
    Signed-off-by: wgxoyun <wgxoyun@cn.ibm.com>

--- zvmsdk/dist.py
+++ zvmsdk/dist.py
@@ -1479,6 +1479,11 @@ class sles12(sles):
         return activeIP_str
 
 
+class sles15(sles12):
+    """docstring for sles15"""
+    pass
+
+
 class ubuntu(LinuxDist):
     def create_network_configuration_files(self, file_path, guest_networks,
                                            first, active=False):
@@ -1786,7 +1791,7 @@ class LinuxDistManager(object):
 
     def _parse_release(self, os_version, distro, remain):
         supported = {'rhel': ['6', '7', '8'],
-                     'sles': ['11', '12'],
+                     'sles': ['11', '12', '15'],
                      'ubuntu': ['16'],
                      'rhcos': ['4']}
         releases = supported[distro]
